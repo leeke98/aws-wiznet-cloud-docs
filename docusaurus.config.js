c82d852a189fd3d8aff8ca2134e06f30ee786243
dlruddms5619@gmail.com <dlruddms5619@gmail.com>
2022-10-20 13:51:27 +0900

    Modify baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   title: "WIZnet Cloud Docs",
   tagline: "Guides and Docs for wizcloud.io", // 나중에 도메인 변경
   url: "https://leeke98.github.io", // docs 도메인으로 변경
-  baseUrl: "/",
+  baseUrl: "/aws-wiznet-cloud-docs/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/wizfavicon.ico",
